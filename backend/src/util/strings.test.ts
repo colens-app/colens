a9844e251e9581230292729423a961bb48b31ae3
William Oldham <github@binaryoverload.co.uk>
2026-04-25 18:12:00 +0100

    toCamelCase now handles trailing/repeat separators

--- backend/src/util/strings.test.ts
+++ backend/src/util/strings.test.ts
@@ -1,16 +1,30 @@
-import { expect, test } from "vitest"
+import { describe, expect, test } from "vitest"
 import { toCamelCase } from "./strings.js"
 
-test("toCamelCase", () => {
-  expect(toCamelCase("Upper-Kebab-Case")).toBe("upperKebabCase")
-  expect(toCamelCase("lower-kebab-case")).toBe("lowerKebabCase")
-  expect(toCamelCase("UpperCamelCase")).toBe("upperCamelCase")
-  expect(toCamelCase("lowerCamelCase")).toBe("lowerCamelCase")
+describe("toCamelCase", () => {
+  test("handles normal cases", () => {
+    expect(toCamelCase("Upper-Kebab-Case")).toBe("upperKebabCase")
+    expect(toCamelCase("lower-kebab-case")).toBe("lowerKebabCase")
+    expect(toCamelCase("UpperCamelCase")).toBe("upperCamelCase")
+    expect(toCamelCase("lowerCamelCase")).toBe("lowerCamelCase")
 
-  // Real cases
-  expect(toCamelCase("Date")).toBe("date")
-  expect(toCamelCase("Valid-Until")).toBe("validUntil")
-  expect(toCamelCase("Acquire-By-Hash")).toBe("acquireByHash")
-  expect(toCamelCase("SHA256")).toBe("sha256")
-  expect(toCamelCase("MD5Sum")).toBe("md5Sum")
+    // Real cases
+    expect(toCamelCase("Date")).toBe("date")
+    expect(toCamelCase("Valid-Until")).toBe("validUntil")
+    expect(toCamelCase("Acquire-By-Hash")).toBe("acquireByHash")
+    expect(toCamelCase("SHA256")).toBe("sha256")
+    expect(toCamelCase("MD5Sum")).toBe("md5Sum")
+  })
+
+  test("handles trailing separator", () => {
+    expect(toCamelCase("Foo-")).toBe("foo")
+  })
+
+  test("handles consecutive separators", () => {
+    expect(toCamelCase("Foo--Bar")).toBe("fooBar")
+  })
+
+  test("handles leading separator", () => {
+    expect(toCamelCase("-Foo")).toBe("foo")
+  })
 })
--- backend/src/util/strings.ts
+++ backend/src/util/strings.ts
@@ -1,5 +1,6 @@
 export function toCamelCase(s: string) {
   return s.split(/[-_]|(?<=[a-z])(?=[A-Z])|(?<=\d)(?=[A-Z])/)
+    .filter(s => s.length > 0)
     .map((w, i) => i === 0 ? w.toLowerCase() : w[0].toUpperCase() + w.slice(1).toLowerCase())
     .join("")
 }
