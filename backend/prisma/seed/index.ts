2e177f6f6941dab912f94484cd9033243313b851
William Oldham <github@binaryoverload.co.uk>
2026-05-03 13:47:32 +0100

    Add basic seeding for distros

---
+++ backend/prisma/seed/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config"
+import { Pool } from "pg"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../../src/generated/prisma/client"
+import { seedUbuntuReleases, seedDistros, seedDebianReleases } from "./packages"
+
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+async function main() {
+  await seedDistros(prisma)
+  await seedUbuntuReleases(prisma)
+  await seedDebianReleases(prisma)
+}
+
+await prisma.$connect()
+try {
+  await main()
+} catch (e) {
+  console.error("Error seeding database:", e)
+} finally {
+  await prisma.$disconnect()
+  await pool.end()
+}
